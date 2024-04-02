fc19def851e15c94f2d79a332ee29bb5240f67a9
lgilton <gilton@bc.edu>
2024-04-02 17:27:07 -0400

    YAY wind!
    live love turbines

--- FinalProject.m
+++ FinalProject.m
@@ -1,14 +1,31 @@
+
+addpath("/Users/lgilton/Documents/GitHub/final-project-sofie-lindsay-annie/archive (2)")
 data1 = readtable("millerkeith2018data1.csv");
 data2 = readtable("millerkeith2018data2.csv");
-data1 = sortrows(data1);
-lat = data1(:,3)
-lon = data1(:,2)
+filename = 'data.nc'
+ncdisp(filename);
 
+windu = ncread(filename,'u10');
+
+windv = ncread(filename,'v10');
+
+totalwindspeed = sqrt((windu .* windu) + (windv .* windv));
+
+% data1 = sortrows(data1);
+lat = table2array(data1(:,3));
+lon = table2array(data1(:,2));
+netgen = table2array(data1(:,6));
 %https://www.kaggle.com/datasets/mrmorj/wind-plant-data
 
-figure(1); clf
-worldmap USA
-contourfm(lat,lon, data1(:,6)','linecolor','none');
-colorbar
-geoshow('landareas.shp','FaceColor','black')
-title("Net Energy Generation")
+% surf(lat,lon,totalwindspeed(:,:,1));
+
+% figure(1); clf
+% usamap conus
+% geoshow('landareas.shp','FaceColor','#77AC30')
+% plotm(lat,lon,'m.','MarkerSize',15)
+% scatterm(lat,lon,netgen)
+
+% % geoplot(data1,"Latitude","Longitude")
+% % hold off
+% % contourfm(lat, lon);
+% % colorbar
